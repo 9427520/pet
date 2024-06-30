54d735e727ba7665955f8d78708adfa00633447f
Eric <1ric77@gmail.com>
2024-07-01 03:27:27 +0800

    修改上傳檔案至Google Storage

--- qn/app.py
+++ qn/app.py
@@ -2,11 +2,11 @@ import os
 import json
 from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
-from datetime import datetime, timedelta
-import smtplib
+from datetime import datetime
+from google.cloud import storage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import requests
+import smtplib
 
 app = Flask(__name__)
 
@@ -29,14 +29,18 @@ users_collection = db['users']
 pets_collection = db['petfile']
 questionnaire_collection = db['questionnaire']
 
+# Google Cloud Storage 設置
+service_account_json = 'lbgcs.json'
+client = storage.Client.from_service_account_json(service_account_json)
+bucket = client.bucket('elbgcs')
+
 # Google Gmail SMTP 設置
 gmail_user = 'petcarewy@gmail.com'
 gmail_password = 'qjecpaojdaswjssg'
 
-@app.route('/qn/send_vaccine_reminders', methods=['GET'])
+@app.route('/pet/send_vaccine_reminders', methods=['GET'])
 def send_vaccine_reminders():  
     today = datetime.now()
-    # 測試今日發送
     reminder_date = today
 
     # 查詢petfile集合中的n_va_re日期為今天的文件
@@ -50,7 +54,7 @@ def send_vaccine_reminders():
     return "Vaccine reminders sent!"
 
 def send_email(to_email, pet_name, vaccine_date):
-    subject = "【寵物疫苗施打提醒!!!】"
+    subject = "寵物疫苗提醒"
     body = f"親愛的寵物主人，\n\n這是一封提醒信，您的寵物 {pet_name} 預定在 {vaccine_date} 進行疫苗接種。請確保提前安排好相關事宜。\n\n此致\n毛起來健檢團隊"
 
     msg = MIMEMultipart()
@@ -72,41 +76,69 @@ def send_email(to_email, pet_name, vaccine_date):
 
 # LINE LIFF 設置
 LIFF_ID = '2005466366-WOBjGlqG'
-@app.route('/qn')
+@app.route('/qn', methods=['GET'])
 def index():
     return render_template('index.html', liff_id=LIFF_ID)
 
 @app.route('/qn/submit_form', methods=['POST'])
 def submit_form():
-    data = request.get_json()
-    user_id = data.get('user_id')
-    form_data = data.get('form_data')
-    
-    if user_id and form_data:
+    try:
+        # 轉換為字典並保留所有值
+        data = request.form.to_dict(flat=False)
+        #print(f"Received form data: {data}")
+        user_id = data.get('user_id', [None])[0]
+        user_email = data.get('user_email', [None])[0]
+        form_data = {key: value if len(value) > 1 else value[0] for key, value in data.items() if key not in ['user_id', 'user_email']}
+        
+        if not user_id:
+            print("Missing user_id")
+            return jsonify({"status": "error", "message": "Missing user_id"}), 400
+        
+        # 合併多選字段為字符串
+        for key, value in form_data.items():
+            if isinstance(value, list):
+                form_data[key] = ', '.join(value)
+
+        if 'report_photos' in request.files:
+            file = request.files['report_photos']
+            current_time = datetime.now().strftime("%y%m%d%H%M%S")
+            file_name = f"{user_id}_{current_time}.png"
+            questionnaire_blob = bucket.blob(f'pet_qn/{file_name}')
+            questionnaire_blob.upload_from_file(file, content_type=file.content_type)
+            form_data['report_photo_url'] = questionnaire_blob.public_url
+            print(f"Uploaded photo URL: {form_data['report_photo_url']}")
+        
         questionnaire_data = {
             "user_id": user_id,
+            "user_email": user_email,
             **form_data
         }
         questionnaire_collection.insert_one(questionnaire_data)
+        print("Inserted questionnaire data into MongoDB")
         return jsonify({"status": "success"}), 200
-    else:
-        return jsonify({"status": "error", "message": "Missing data"}), 400
+    except Exception as e:
+        print(f"Error processing form submission: {str(e)}")
+        return jsonify({"status": "error", "message": "Internal server error"}), 500
 
 @app.route('/qn/login', methods=['POST'])
 def login():
-    access_token = request.json.get('access_token')
-    if not access_token:
-        return jsonify({"status": "error", "message": "Access token is required"}), 400
-    
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-    response = requests.get('https://api.line.me/v2/profile', headers=headers)
-    if response.status_code == 200:
-        profile = response.json()
-        return jsonify({"status": "success", "user_id": profile['userId']}), 200
-    else:
-        return jsonify({"status": "error", "message": "Invalid access token"}), 400
+    try:
+        access_token = request.json.get('access_token')
+        if not access_token:
+            return jsonify({"status": "error", "message": "Access token is required"}), 400
+        
+        headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
+        response = requests.get('https://api.line.me/v2/profile', headers=headers)
+        if response.status_code == 200:
+            profile = response.json()
+            return jsonify({"status": "success", "user_id": profile['userId']}), 200
+        else:
+            return jsonify({"status": "error", "message": "Invalid access token"}), 400
+    except Exception as e:
+        print(f"Error during login: {str(e)}")
+        return jsonify({"status": "error", "message": "Internal server error"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
